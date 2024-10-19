a579431b50408df4e4a566c67301634ff6958129
Robin <robin.helsen@student.ucll.be>
2024-10-19 17:31:10 +0200

    Changed all classes + Made tests for Setup, Images, hardware...

--- back-end/model/images.ts
+++ back-end/model/images.ts
@@ -1,11 +1,17 @@
-export class images {
-    readonly url: String;
+export class Images {
+    private url: String; // Primary key
 
     constructor(image: { url: String }) {
         this.url = image.url;
     }
 
+    // GETTERS
     getUrl(): String {
         return this.url;
+    }
+
+    // SETTERS
+    setUrl(url: String): void {
+        this.url = url;
     }
 }
